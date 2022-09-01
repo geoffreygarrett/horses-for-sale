a28b48d0274443a52b30c20751b4205a1dff63e0
geoffreygarrett <g.h.garrett13@gmail.com>
2022-09-01 07:49:57 +0000

    Build: (2edfc23) initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-
+// https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript/53880407#53880407
 const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
     v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
     )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
@@ -8,7 +8,19 @@ const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
 document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
     const table = th.closest('table');
     const tbody = table.querySelector('tbody');
+
+    // add up arrow for last clicked th if ascending, down arrow if descending
+    //  remove previous arrows
+    document.querySelectorAll('th').forEach(th => {
+        th.innerHTML = th.innerHTML.replace("▲", "");
+        th.innerHTML = th.innerHTML.replace("▼", "");
+    });
+    th.innerHTML += this.asc ? " &#9660;" : " &#9650;";
+    // breed ▼ ▲ ▼ ▲ ▼ ▲
     Array.from(tbody.querySelectorAll('tr'))
       .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
       .forEach(tr => tbody.appendChild(tr) );
 })));
+
+
+
